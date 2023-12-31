99503f963f0095e5e4cd95ebb3187b4fc4de240a
TemesgenD1012 <temesgendebebe1921@gmail.com>
2023-12-31 22:29:56 +0300

    soul-stories

---
+++ src/app/joindtheroom/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Joined() {
+  return (
+    <div>Joined</div>
+  )
+}
+
+export default Joined
---
+++ src/components/navbarout/NavbarOut.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavbarOut() {
+  return (
+    <div>NavbarOut</div>
+  )
+}
+
+export default NavbarOut
